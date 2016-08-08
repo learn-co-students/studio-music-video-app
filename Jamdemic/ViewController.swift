03b17322abca632207c9bf6d20feafb51b4650bd
jellybeanjohnny <mamerige@icloud.com>
2016-08-08 17:04:36 -0400

    removing extraneous code from the viewController

--- Jamdemic/ViewController.swift
+++ Jamdemic/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        SpotifyAPIOAuthClient.refreshSpotifyAccessToken { (token) in
-            
-        }
+        
     }
 
 }
