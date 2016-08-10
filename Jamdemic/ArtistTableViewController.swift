a949da4e4a4dec4325a67585db9262ca41486731
dochonglo <ismaelbarry777@hotmail.com>
2016-08-10 11:01:35 -0400

    Added ArtistTableViewController file. Refactored code between GenreTableViewController and ArtistTableViewController files for efficency, readability, and code reuseability. Artists can now be populated in the table view.

---
+++ Jamdemic/ArtistTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistTableViewController.swift
+//  Jamdemic
+//
+//  Created by Ismael Barry on 8/10/16.
+//  Copyright © 2016 crocodile. All rights reserved.
+//
+
+import Cocoa
+
+class ArtistTableViewController: UITableViewController {
+
+}
